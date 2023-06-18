67c28c417a585a7673a9e023f76f542ae14838f8
민정 <55127180+minjeong19@users.noreply.github.com>
2023-06-18 19:14:33 +0900

    css&js실습 -계산기, 이미지랜덤, 슬라이더

---
+++ minjeong/실습/3-4.js
@@ -0,0 +1,8 @@
+let text = "abababdjdjskf";
+let length = text.length;
+let index = text.indexOf("kf", 9);
+console.log(length);
+console.log(index);
+
+const fruits = ["banana", "orange", "applel", "mango"];
+console.log(fruits.toString);
